6964d71e3d8dc012f538e67dd73a4901c63b10aa
bulaf88 <bulaf88@abv.bg>
2019-02-24 21:28:08 +0200

    ok

--- ClubParty/Program.cs
+++ ClubParty/Program.cs
@@ -2,9 +2,9 @@
 
 namespace ClubParty
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
             
         }
